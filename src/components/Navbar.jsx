bc27980867785d626cefcd8c57bed59b2164b02a
THEOU JEFFINE <jefftheou@gmail.com>
2025-10-07 23:28:18 +0530

    added logo to navabar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
+import Logo from "../assets/Logo.png";
 
 export default function Navbar() {
   const [open, setOpen] = useState(false);
@@ -11,10 +12,19 @@ export default function Navbar() {
 
   return (
     <header className="sticky top-0 z-50 bg-white/80 backdrop-blur border-b border-slate-300">
-      <div className="mx-auto max-w-7xl px-4 py-4 flex items-center">
-        <div className="items-center justify-start ml-4 w-32">
-          <NavLink to="/" className="text-xl font-semibold tracking-tight">
-            Anfed
+      <div className="mx-auto max-w-7xl px-4 py-2 flex items-center">
+        <div className="items-center justify-center ml-4 h-12 md:h-14">
+          <NavLink
+            to="/"
+            className="text-xl font-semibold tracking-tight flex items-center gap-2">
+            <img
+              src={Logo}
+              alt="Anfed"
+              className="h-8 md:h-10 items-center w-auto object-contain md:scale-110"
+            />
+            <span className="text-lg md:text-xl font-semibold tracking-wider text-gray-800">
+              ANFED
+            </span>
           </NavLink>
         </div>
 
@@ -41,16 +51,14 @@ export default function Navbar() {
         <div className="flex justify-end ml-8">
           <NavLink
             to="/contact"
-            className="bg-[#448800] font-bold rounded-full transform-gpu transition-transform duration-300 ease-out hover:scale-105 active:scale-100 focus:outline-none px-4 py-2 text-white text-center"
-          >
+            className="bg-[#448800] font-bold rounded-full transform-gpu transition-transform duration-300 ease-out hover:scale-105 active:scale-100 focus:outline-none px-4 py-2 text-white text-center">
             Contact
           </NavLink>
 
           <button
             onClick={() => setOpen((v) => !v)}
             className="md:hidden p-2 rounded-md hover:bg-slate-100 ml-3"
-            aria-label="Toggle menu"
-          >
+            aria-label="Toggle menu">
             <svg
               width="24"
               height="24"
@@ -58,8 +66,7 @@ export default function Navbar() {
               fill="none"
               stroke="currentColor"
               strokeWidth="2"
-              className="text-slate-700"
-            >
+              className="text-slate-700">
               <path d="M4 6h16M4 12h16M4 18h16" />
             </svg>
           </button>
