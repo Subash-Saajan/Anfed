2c113629ee609b382be209f6c51529b44c05f6bd
THEOU JEFFINE <jefftheou@gmail.com>
2025-10-11 01:30:32 +0530

    added a

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -165,7 +165,7 @@ export default function Home() {
         </div>
         <div className="absolute bottom-20 left-10 text-white z-10">
           <h2 className="text-4xl font-bold">
-            numan Nadhi Fed <br />
+            Anuman Nadhi Fed <br />
             Farmer Producer Company Ltd
           </h2>
         </div>
